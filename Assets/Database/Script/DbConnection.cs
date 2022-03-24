b9253bd2eed76b006cdc8bbb4a87e05e521e5e14
rayandu924 <rayandu924@gmail.com>
2022-03-24 00:04:02 +0000

    Switch from MySQL to MongoDB, Creating DataBase,Start Connecting DataBase to Unity

---
+++ Assets/Database/Script/DbConnection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+public class DbConnection : MonoBehaviour {
+
+	const string dbUrl = "mongodb+srv://123:123@cluster0.vgila.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+	public string dbName;
+	public string dbCollection;
+
+	MongoClient client = new MongoClient(dbUrl);
+	IMongoDatabase database;
+	IMongoCollection<BsonDocument> collection;
+	// Use this for initialization
+	void Start () {
+		database = client.GetDatabase(dbName);
+		collection = database.GetCollection<BsonDocument>(dbCollection);
+		foreach (var item in client.ListDatabaseNames().ToList())
+		{
+			Debug.Log(item);
+		}
+	}
+}
